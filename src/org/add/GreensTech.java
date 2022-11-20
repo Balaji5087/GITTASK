0ffc1dcf29633ff7a213297faeac1efd49a2c4d3
Balaji5087 <balaji.5087@gmail.com>
2022-11-20 22:47:02 +0530

    Codes

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,29 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("greensOmr");
+	}
+	private void greensAdayar() {
+		System.out.println("greensAdayar1");
+	}
+	private void greensTambaram() {
+		System.out.println("greensTambaram");
+	}
+	private void greensVelacherry() {
+		System.out.println("greensVelacherry");
+		
+	}
+	private void greensAnnaNagar() {
+		System.out.println("greensAnnaNagar");
+	}
+	public static void main(String[] args) {
+		GreensTech s=new GreensTech();
+		s.greensOmr();
+		s.greensAdayar();
+		s.greensTambaram();
+		s.greensVelacherry();
+		s.greensAnnaNagar();
+	}
+
+}
